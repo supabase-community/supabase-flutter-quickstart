3e2462af68472b9b175e3c1495d30b3fe85a01cb
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2021-07-27 19:31:15 +0900

    chore: Added comments to auth_required_state

--- lib/components/auth_required_state.dart
+++ lib/components/auth_required_state.dart
@@ -5,6 +5,7 @@ class AuthRequiredState<T extends StatefulWidget>
     extends SupabaseAuthRequiredState<T> {
   @override
   void onUnauthenticated() {
+    /// Users will be sent back to the LoginPage if they sign out.
     Navigator.of(context).pushNamedAndRemoveUntil('/login', (route) => false);
   }
 }
